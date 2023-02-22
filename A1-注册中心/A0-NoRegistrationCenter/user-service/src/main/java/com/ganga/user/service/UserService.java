3f3d82aaca92af1a3f9883a3c31b854bce89c1c5
尴尬酱 <2282514478@qq.com>
2023-02-22 23:13:28 +0800

    //未使用注册中心

---
+++ A1-注册中心/A0-NoRegistrationCenter/user-service/src/main/java/com/ganga/user/service/UserService.java
@@ -0,0 +1,17 @@
+package com.ganga.user.service;
+
+import com.ganga.user.mapper.UserMapper;
+import com.ganga.user.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User queryById(Long id) {
+        return userMapper.findById(id);
+    }
+}
